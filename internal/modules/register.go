9406b803bb3aca63491e9c3504d9d4d90843e32b
u400r <uk.hitoshi@gmail.com>
2022-09-20 00:58:38 +0900

    Implement flag register to represent status of cpu

--- internal/modules/register.go
+++ internal/modules/register.go
@@ -19,6 +19,14 @@ type Counter[T BitSignal] interface {
 	WritableRegister[T]
 }
 
+type Flag[T BitSignal] interface {
+	WritableRegister[T]
+	Get(nbit uint) bool
+	Set(nbit uint)
+	Clear(nbit uint)
+	Change(nbit uint, flag bool)
+}
+
 type Register[T BitSignal] struct {
 	data T
 }
@@ -44,3 +52,33 @@ func (r *Register[T]) Increment() {
 func (r *Register[T]) Decrement() {
 	r.data -= 1
 }
+
+func (r *Register[T]) Get(nbit uint) bool {
+	return (r.data>>nbit)&0x1 == 0x1
+}
+
+func (r *Register[T]) Set(nbit uint) {
+	r.data = r.data | 1<<nbit
+}
+
+func (r *Register[T]) Clear(nbit uint) {
+	r.data = r.data & ^(1 << nbit)
+}
+
+func (r *Register[T]) Change(nbit uint, flag bool) {
+	if flag {
+		r.Set(nbit)
+	} else {
+		r.Clear(nbit)
+	}
+}
+
+const (
+	CARRY     uint = 0
+	ZERO      uint = 1
+	INTERRUPT uint = 2
+	DECIMAL   uint = 3
+	BLEAK     uint = 4
+	OVERFLOW  uint = 6
+	NEGATIVE  uint = 7
+)
