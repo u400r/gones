837401f92b19e1807a33e247420554ef585716c9
u400r <uk.hitoshi@gmail.com>
2022-09-22 16:29:33 +0900

    Show inner state of cpu each at operation completed

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -7,15 +7,24 @@ import (
 )
 
 type Cpu struct {
+	// registers
 	aRegister              modules.WritableRegister[uint8]
 	bRegister              modules.WritableRegister[uint8]
 	xRegister              modules.WritableRegister[uint8]
 	yRegister              modules.WritableRegister[uint8]
 	programCounterRegister modules.Counter[uint16]
 	statusRegister         modules.Flag[uint8]
-	stack                  *modules.Stack[uint8, uint8, uint16]
-	ram                    modules.Writable[uint8, uint16]
-	clock                  Clock
+	// stack
+	stack *modules.Stack[uint8, uint8, uint16]
+	// clock
+	clock *Clock
+
+	// outside component
+	ram modules.Writable[uint8, uint16]
+
+	// inner state
+	instructionAddress uint16
+	instructionOpecode uint8
 }
 
 func NewCpu(memory modules.Writable[uint8, uint16]) *Cpu {
@@ -28,20 +37,22 @@ func NewCpu(memory modules.Writable[uint8, uint16]) *Cpu {
 		statusRegister:         modules.NewRegister(uint8(0)),
 		stack:                  modules.NewStack(memory, uint8(0)),
 		ram:                    memory,
-		clock:                  &clock{},
+		clock:                  &Clock{},
 	}
 	c.initDecoder()
 	return c
 }
 
-type Clock interface {
-	Tick()
-}
-type clock struct {
+type Clock struct {
+	Cycles int
 }
 
-func (c *clock) Tick() {
+func (c *Clock) Tick() {
+	c.Cycles += 1
+}
 
+func (c *Clock) GetCycles() int {
+	return c.Cycles
 }
 
 type AddressingMode func(c *Cpu) *uint16
@@ -52,11 +63,28 @@ func (c *Cpu) Process() {
 	if op == nil || mode == nil {
 		panic("something went wrong")
 	}
-	fmt.Printf("%v %v\n", op.Opecode(), mode.GetModeString())
 
 	addr := mode.GetAddress(c)
 	op.Do(c, addr)
 	c.programCounterRegister.Increment()
+	c.printState(op, mode)
+
+}
+
+func (c *Cpu) printState(op Operatable, mode Addressing) {
+	first, second := mode.GetOperandBytes(c)
+
+	if first == nil {
+		fmt.Printf("%X  %X        %v %v\n", c.instructionAddress, c.instructionOpecode, op.Opecode(), c.clock.Cycles)
+
+	} else {
+		if second == nil {
+			fmt.Printf("%4X  %2X %2X     %v %v\n", c.instructionAddress, c.instructionOpecode, *first, op.Opecode(), c.clock.Cycles)
+
+		} else {
+			fmt.Printf("%4X  %2X %2X %2X  %v %v\n", c.instructionAddress, c.instructionOpecode, *first, *second, op.Opecode(), c.clock.Cycles)
+		}
+	}
 
 }
 
@@ -65,5 +93,7 @@ func (c *Cpu) IsWaitOneClock() bool {
 }
 
 func (c *Cpu) fetch() uint8 {
-	return c.ram.Read(c.programCounterRegister.Read())
+	c.instructionAddress = c.programCounterRegister.Read()
+	c.instructionOpecode = c.ram.Read(c.instructionAddress)
+	return c.instructionOpecode
 }
